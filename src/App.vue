63dbd622ee53d38cbdfe538ee4ad0dd133a4a11b
Mihailo Tomisic <mihailotomisic@MacBook-Pro-6.local>
2019-05-16 21:04:24 +0200

    iitial commit

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <MainNav />
     <router-view/>
   </div>
 </template>
+
+<script>
+import MainNav from '@/components/navigation/MainNav'
+
+export default {
+  components: {
+    MainNav
+  }
+}
+</script>
 
 <style>
 #app {
