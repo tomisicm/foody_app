98782e69709312f5d300eab5d7e8cddafb823775
Mihailo Tomisic <mihailotomisic@MacBook-Pro-6.local>
2019-05-18 23:39:32 +0200

    a

--- src/router.js
+++ src/router.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
-import Facility from './views/Facility.vue'
+import Home from './views/Home'
+import Facility from './views/Facility'
+import Review from './views/Review'
 
 Vue.use(Router)
 
@@ -30,16 +31,7 @@ export default new Router({
       path: '/reviews',
       name: 'reviews',
       props: true,
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: Review
     }
   ]
 })
