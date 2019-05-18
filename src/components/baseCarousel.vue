562314f797cc6b1b5406bc78a1af9517d1b46277
Mihailo Tomisic <mihailotomisic@MacBook-Pro-6.local>
2019-05-18 12:12:59 +0200

    object page basic layout

---
+++ src/components/baseCarousel.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="carousel slide carousel-fade" data-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="https://images.pexels.com/photos/6267/menu-restaurant-vintage-table.jpg?auto=compress&cs=tinysrgb&dpr=2&w=500" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="https://images.pexels.com/photos/262978/pexels-photo-262978.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="https://images.pexels.com/photos/67468/pexels-photo-67468.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" class="d-block w-100" alt="...">
+      </div>
+    </div>
+    <a class="carousel-control-prev" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</template>
--- src/views/Facility.vue
+++ src/views/Facility.vue
@@ -1,28 +1,44 @@
 <template>
   <b-container>
-    <div>Giben pbject</div>
+    <div>
+      <h1>Giben pbject</h1>
+    </div>
+    <baseCarousel class="my-4" />
 
+    <div class="nav justify-content-center">
+
+      <b-card no-body class="my-4 w-100">
+        <b-tabs card fill class="nav-item">
+          <b-tab title="Reviews" active>
+            <b-card-text>Reviews</b-card-text>
+          </b-tab>
+          <b-tab title="Comments">
+            <b-card-text>Comments</b-card-text>
+          </b-tab>
+        </b-tabs>
+      </b-card>
+
+    </div>
   </b-container>
 </template>
 
 <script>
+import baseCarousel from '@/components/baseCarousel'
+
 export default {
   data () {
     return {
-
+      data: [
+      ]
     }
+  },
+
+  components: {
+    baseCarousel
   }
 }
 </script>
 
 <style>
-.example-slide {
-  align-items: center;
-  background-color: #666;
-  color: #999;
-  display: flex;
-  font-size: 1.5rem;
-  justify-content: center;
-  min-height: 10rem;
-}
+
 </style>
