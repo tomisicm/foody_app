17985dc3249757ce518818c86a0ac47312886f43
Mihailo Tomisic <mihailotomisic@MacBook-Pro-6.local>
2019-05-27 10:39:02 +0200

    pagination

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,6 +6,27 @@
       </b-col>
 
       <b-col cols="8">
+        <b-row>
+          <div cols="4">
+            <b-dropdown
+              split
+              variant="primary"
+              text="10"
+              class="m-2"
+              >
+              <b-dropdown-item href="#">25</b-dropdown-item>
+              <b-dropdown-item href="#">50</b-dropdown-item>
+              <b-dropdown-item href="#">100</b-dropdown-item>
+            </b-dropdown>
+          </div>
+          <div cols="8">
+            <b-pagination
+              v-model="currentPage"
+              :total-rows="total"
+              :per-page="limit"
+            />
+          </div>
+        </b-row>
         <b-table
           :items="items"
           :fields="fields"
@@ -27,6 +48,7 @@ export default {
 
   data () {
     return {
+      currentPage: 1,
       limit: 10,
       page: 1,
       pages: null,
@@ -79,3 +101,9 @@ export default {
   components: { FilterObjects }
 }
 </script>
+
+<style>
+ul.pagination.b-pagination.pagination-md {
+  margin-top: .5rem;
+}
+</style>
