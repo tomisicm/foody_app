decb62ff7bfb1e90d4f53a6f4850a49c8dd96d0f
Mihailo Tomisic <mihailotomisic@MacBook-Pro-6.local>
2019-05-27 16:00:31 +0200

    removing console log

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -130,7 +130,6 @@ export default {
     log (event) {
       let routeData = this.$router.resolve({name: 'catering', params: {id: event._id}})
       window.open(routeData.href, '_blank')
-     console.log(routeData)
     }
 
   },
