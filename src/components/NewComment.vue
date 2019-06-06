9949270fffc864edd21ab7846497caa8988c5c07
Mihailo Tomisic <mihailotomisic@MacBook-Pro-6.local>
2019-06-06 18:43:00 +0200

    lint

--- src/components/NewComment.vue
+++ src/components/NewComment.vue
@@ -1,24 +1,24 @@
 <template>
   <div v-if="isSignedIn">
-  <b-card no-body class="my-2">
-    <b-form-textarea
-      v-model="body"
-      placeholder="New comment..."
-      rows="2"
-      max-rows="5"
-      :style="{ overflowY: 'none' }"
-    />
-  </b-card>
-  <b-container>
-    <b-row>
-      <b-btn
-        v-if="body"
-        class="mr-auto my-2"
-        @click="handleCreateComment">
-        Send</b-btn>
-    </b-row>
-  </b-container>
-</div>
+    <b-card no-body class="my-2">
+      <b-form-textarea
+        v-model="body"
+        placeholder="New comment..."
+        rows="2"
+        max-rows="5"
+        :style="{ overflowY: 'none' }"
+      />
+    </b-card>
+    <b-container>
+      <b-row>
+        <b-btn
+          v-if="body"
+          class="mr-auto my-2"
+          @click="handleCreateComment">
+          Send</b-btn>
+      </b-row>
+    </b-container>
+  </div>
 </template>
 
 <script>
