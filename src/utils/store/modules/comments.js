23255af4bd4fbc5dfa447c5234ae3cbcd556dc89
Mihailo Tomisic <mihailotomisic@MacBook-Pro-6.local>
2019-06-13 12:36:04 +0200

    basic

---
+++ src/utils/store/modules/comments.js
@@ -0,0 +1,59 @@
+import commentService from '@/utils/services/comment-service'
+
+function getInitialState () {
+  return {
+    comments: []
+  }
+}
+
+const state = getInitialState()
+
+const mutations = {
+  SET_DATA (state, comments) {
+    state.comments = comments
+  }
+}
+
+const actions = {
+  async createComment ({ commit }, comment) {
+    try {
+      const { data } = await commentService.createComment()
+
+      // commit('UPDATE_DATA', data.docs)
+    } catch (error) {
+      throw error
+    }
+  },
+
+  async deleteComment ({ commit }, commentId) {
+    try {
+      const { data } = await commentService.deleteComment(commentId)
+    } catch (error) {
+      throw error
+    }
+
+    // commit('REMOVE_DATA', data.docs)
+  },
+
+  async getComments ({ commit }, itemId) {
+    try {
+      const { data } = await commentService.getCommentsByItemId(itemId)
+
+      commit('SET_DATA', data.docs)
+    } catch (error) {
+      throw error
+    }
+  }
+}
+
+const getters = {
+  comments: state => state.comments
+}
+
+export const commentStore = {
+  namespaced: true,
+  state,
+  actions,
+  mutations,
+  getters
+}
