a9d619b8043c851ac772cc0e54c066c74cb64efd
Mihailo Tomisic <mihailotomisic@MacBook-Pro-6.local>
2019-06-14 20:34:39 +0200

    Profile basic laylout

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,3 +1,35 @@
 <template>
-  <b-container>This is profile page</b-container>
+  <b-container class="mt-2">
+    Profile Page:
+    <b-form>
+      <b-row md="6" class="my-1">
+        <b-form-group label-cols-sm="3" label="Name" class="mb-0">
+          <b-input-group>
+            <b-form-input v-model="form.username" placeholder="Username"></b-form-input>
+          </b-input-group>
+        </b-form-group>
+      </b-row>
+
+      <b-row md="6" class="my-1">
+        <b-form-group label-cols-sm="3" label="Profession" class="mb-0">
+          <b-input-group>
+            <b-form-input v-model="form.username" placeholder="Profession"></b-form-input>
+          </b-input-group>
+        </b-form-group>
+      </b-row>
+    </b-form>
+  </b-container>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      form: {
+        username: '',
+        profession: ''
+      }
+    }
+  }
+}
+</script>
