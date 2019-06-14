2ebead7ff3c71f365ae9aecddb82998b1cec428f
Mihailo Tomisic <mihailotomisic@MacBook-Pro-6.local>
2019-06-14 22:33:22 +0200

    the beginning

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -2,21 +2,39 @@
   <b-container class="mt-2">
     Profile Page:
     <b-form>
-      <b-col md="6" class="my-1">
-        <b-form-group label-cols-sm="3" label="Name" class="mb-0">
-          <b-input-group>
-            <b-form-input v-model="form.username" placeholder="Username"></b-form-input>
-          </b-input-group>
-        </b-form-group>
-      </b-col>
+      <b-row md="8">
+        <b-col>
+          <b-col class="my-2">
+            <b-form-group label-cols-sm="3" label="Name" class="mb-0">
+              <b-input-group>
+                <b-form-input v-model="form.username" placeholder="Username"></b-form-input>
+              </b-input-group>
+            </b-form-group>
+          </b-col>
 
-      <b-col md="6" class="my-1">
-        <b-form-group label-cols-sm="3" label="Profession" class="mb-0">
-          <b-input-group>
-            <b-form-input v-model="form.username" placeholder="Profession"></b-form-input>
-          </b-input-group>
-        </b-form-group>
-      </b-col>
+          <b-col class="my-2">
+            <b-form-group label-cols-sm="3" label="Profession" class="mb-0">
+              <b-input-group>
+                <b-form-input
+                  v-model="form.username"
+                  placeholder="Profession"></b-form-input>
+              </b-input-group>
+            </b-form-group>
+          </b-col>
+
+          <b-col class="my-2">
+            <b-form-group label="Avatar" class="mb-0">
+              <b-form-file
+                accept="image/jpeg, image/png, image/gif"
+                placeholder="Choose picture">
+              </b-form-file>
+            </b-form-group>
+          </b-col>
+        </b-col>
+
+        <b-col>dicc</b-col>
+
+      </b-row>
     </b-form>
   </b-container>
 </template>
