ff2d8eba9bebb7f3e77b466d0d3509cb050da748
Mihailo Tomisic <mihailotomisic@MacBook-Pro-6.local>
2019-06-15 00:44:58 +0200

    basics

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -2,9 +2,9 @@
   <b-container class="mt-2">
     Profile Page:
     <b-form>
-      <b-row md="8">
-        <b-col>
-          <b-col class="my-2">
+      <b-row>
+        <b-col md="8">
+          <b-col class="my-3">
             <b-form-group label-cols-sm="3" label="Name" class="mb-0">
               <b-input-group>
                 <b-form-input v-model="form.username" placeholder="Username"></b-form-input>
@@ -12,7 +12,17 @@
             </b-form-group>
           </b-col>
 
-          <b-col class="my-2">
+          <b-col class="my-3">
+            <b-form-group label-cols-sm="3" label="Email" class="mb-0">
+              <b-input-group>
+                <b-form-input
+                  v-model="form.email"
+                  placeholder="Email"></b-form-input>
+              </b-input-group>
+            </b-form-group>
+          </b-col>
+
+          <b-col class="my-3">
             <b-form-group label-cols-sm="3" label="Profession" class="mb-0">
               <b-input-group>
                 <b-form-input
@@ -21,20 +31,25 @@
               </b-input-group>
             </b-form-group>
           </b-col>
-
-          <b-col class="my-2">
-            <b-form-group label="Avatar" class="mb-0">
-              <b-form-file
-                accept="image/jpeg, image/png, image/gif"
-                placeholder="Choose picture">
-              </b-form-file>
-            </b-form-group>
-          </b-col>
         </b-col>
 
-        <b-col>dicc</b-col>
-
+        <b-col md="4" id="preview">
+          <b-img v-if="selectedFileUrl" width="200" height="200" :src="selectedFileUrl" />
+        </b-col>
       </b-row>
+
+      <b-row md="8" class="mt-3">
+        <b-form-group class="mb-0">
+          <b-form-file
+            @change="onFileSelected"
+            accept="image/jpeg, image/png, image/gif"
+            placeholder="Choose picture"
+          >
+          </b-form-file>
+        </b-form-group>
+        <b-btn @click="handleUpload" class="mx-2" variant="primary">Upload</b-btn>
+      </b-row>
+
     </b-form>
   </b-container>
 </template>
@@ -45,9 +60,37 @@ export default {
     return {
       form: {
         username: '',
-        profession: ''
-      }
+        profession: '',
+        email: ''
+      },
+      selectedFile: null,
+      selectedFileUrl: null
+    }
+  },
+
+  methods: {
+    onFileSelected (event) {
+      this.selectedFile = event.target.files[0]
+
+      this.selectedFileUrl = this.selectedFile && URL.createObjectURL(this.selectedFile)
+    },
+
+    handleUpload () {
+      console.log('uploading')
     }
   }
 }
 </script>
+
+<style>
+#preview {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+#preview > img {
+  max-width: 200px;
+  max-height: 200px;
+}
+</style>
