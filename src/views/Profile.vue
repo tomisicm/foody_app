9c7ae7972a4a25517fe405a17573335d4ab63a82
Mihailo Tomisic <mihailotomisic@MacBook-Pro-6.local>
2019-06-15 08:33:48 +0200

    fuck its hot

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,5 +1,5 @@
 <template>
-  <b-container class="mt-2">
+  <b-container class="my-4">
     Profile Page:
     <b-form>
       <b-row>
@@ -33,9 +33,10 @@
           </b-col>
         </b-col>
 
-        <b-col md="4" id="preview">
+        <!-- <b-col md="4" id="preview">
           <b-img v-if="selectedFileUrl" width="200" height="200" :src="selectedFileUrl" />
-        </b-col>
+        </b-col> -->
+
       </b-row>
 
       <b-row md="8" class="mt-3">
@@ -47,14 +48,40 @@
           >
           </b-form-file>
         </b-form-group>
+        <b-btn @click="cropImage" v-if="selectedFileUrl" style="margin-right: 40px;">Crop</b-btn>
         <b-btn @click="handleUpload" class="mx-2" variant="primary">Upload</b-btn>
       </b-row>
-
     </b-form>
+
+    <div style="width: 400px; height:300px; border: 1px solid gray; display: inline-block;">
+      <vue-cropper
+        ref='cropper'
+        :guides="true"
+        :view-mode="2"
+        drag-mode="crop"
+        :auto-crop-area="0.5"
+        :min-container-width="250"
+        :min-container-height="180"
+        :background="true"
+        :rotatable="true"
+        :src="selectedFileUrl"
+        alt="Source Image"
+        :img-style="{ 'width': '400px', 'height': '300px' }">
+      </vue-cropper>
+    </div>
+    <img :src="cropImg" style="width: 200px; height: 150px; border: 1px solid gray" alt="Cropped Image" />
+    <br/>
+    <br />
+
+    <button @click="cropImage" v-if="selectedFileUrl" style="margin-right: 40px;">Crop</button>
+
   </b-container>
 </template>
 
 <script>
+import VueCropper from 'vue-cropperjs'
+import 'cropperjs/dist/cropper.css'
+
 export default {
   data () {
     return {
@@ -71,13 +98,21 @@ export default {
   methods: {
     onFileSelected (event) {
       this.selectedFile = event.target.files[0]
-
       this.selectedFileUrl = this.selectedFile && URL.createObjectURL(this.selectedFile)
+      this.$refs.cropper.replace(this.selectedFileUrl)
+    },
+
+    cropImage () {
+      this.cropImg = this.$refs.cropper.getCroppedCanvas().toDataURL()
     },
 
     handleUpload () {
       console.log('uploading')
     }
+  },
+
+  components: {
+    VueCropper
   }
 }
 </script>
