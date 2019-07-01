59e4293934d954c7b9769b972d1e7b79ccc1661d
Mihailo Tomisic <mihailotomisic@MacBook-Pro-6.local>
2019-07-01 19:22:06 +0200

    break

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -2,10 +2,22 @@
   <div class="mt-4">
     <ListControll
       :items="menuItems"
-      @additem="handleAddMenuItem"
-      @removeitem="handleRemoveMenuItem"
       :listitemclass="'col-md-4'"
     >
+      <!-- disable this button as long as there is one new item -->
+      <template v-slot:btnfield>
+        <b-row class="ml-3 mb-3" md="2">
+          <b-btn variant="primary" @click="handleAddMenuItem">New Menu Item</b-btn>
+        </b-row>
+      </template>
+      <!-- <template v-slot:inputfield>
+        <div class="col-md-4">
+          <SingleMenuItem
+            :item="newMenuItem"
+          />
+        </div>
+      </template> -->
+
       <template v-slot:listitem={item}>
         <SingleMenuItem
           :item="item"
@@ -23,11 +35,10 @@ import ListControll from '@/components/ListControll'
 export default {
   data () {
     return {
-      newMenuItem: {
-
-      },
+      newMenuItem: Object.assign({}, this.resetNewItem()),
       menuItems: [
         {
+          _id: '23423423423423',
           name: 'Virsla',
           image: 'https://www.donesi.com/images/product/20/166820_m.jpg',
           description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt deleniti est, saepe, voluptas ipsam animi, consequatur ipsum dolorum iure asperiores labore facilis tenetur.',
@@ -36,6 +47,7 @@ export default {
           tag: 'Popular'
         },
         {
+          _id: '23423423423433',
           name: 'Virsla II',
           image: 'https://www.donesi.com/images/product/20/166820_m.jpg',
           description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt deleniti est, saepe, voluptas ipsam animi, consequatur ipsum dolorum iure asperiores labore facilis tenetur.',
@@ -49,10 +61,21 @@ export default {
 
   methods: {
     handleAddMenuItem () {
-
+      this.menuItems.unshift(this.newMenuItem)
     },
     handleRemoveMenuItem () {
 
+    },
+
+    resetNewItem () {
+      return {
+        name: '',
+        image: 'https://www.donesi.com/images/product/20/166820_m.jpg',
+        description: '',
+        portion: '',
+        price: '',
+        tag: ''
+      }
     }
   },
 
