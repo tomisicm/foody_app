e9e14e56d708923580a4b4dcb66fbf9346d941ed
Mihailo Tomisic <mihailotomisic@MacBook-Pro-6.local>
2019-07-08 16:57:32 +0200

    small screens okey big not

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <b-container fluid>
     <b-row>
-      <b-col md="4">
+      <b-col md="4" order="2" order-sm="2" order-md="1">
         <b-row class="ml-1 mr-1">
         <FilterObjects
           :perPage="perPage"
@@ -12,17 +12,16 @@
         </b-row>
       </b-col>
 
-      <b-col md="8">
+      <!-- TODO: if user is logged in -->
+      <b-col md="8" order="1" order-sm="1" class="h-25">
+        <b-nav pills class="my-2 ml-1">
+          <b-nav-item active>All Foody Places</b-nav-item>
+          <b-nav-item :to="{ name: 'addcatering' }">New Foody Place</b-nav-item>
+        </b-nav>
+      </b-col>
 
-        <!-- TODO: if user is logged in -->
-        <b-row class="mb-2">
-          <b-nav pills>
-            <b-nav-item active>All Foody Places</b-nav-item>
-            <b-nav-item :to="{ name: 'addcatering' }">New Foody Place</b-nav-item>
-          </b-nav>
-        </b-row>
-
-        <b-row>
+      <b-col md="8" order="3" order-sm="3">
+        <b-row class="ml-1">
           <div cols="4">
             <b-dropdown
               split
