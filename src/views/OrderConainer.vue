ab993cf700ae80ad879ae9406a8e18fe7928fa7c
Mihailo Tomisic <mihailotomisic@MacBook-Pro-6.local>
2019-07-15 09:14:31 +0200

    TODO REFACTOR MENU

--- src/views/OrderConainer.vue
+++ src/views/OrderConainer.vue
@@ -6,38 +6,57 @@
         <Cart />
       </b-col>
 
+      <!-- REFACTOR MENU ITEM -->
       <b-col md="9">
-        <Menu
-          :orderState="true"
-        />
+        <ListControll
+          :items="menuItems"
+          :listitemclass="'col-md-4'"
+        >
+          <template v-slot:listitem="{item}">
+            <SingleMenuItem
+              :item="item"
+              :orderState="true"
+              @deleteitem="handleDeleteMenuItem($event)"
+            />
+          </template>
+        </ListControll>
       </b-col>
-
     </b-row>
   </div>
 </template>
 
 <script>
-import Menu from '@/components/Menu'
+import cateringService from '@/utils/services/catering-service'
+
 import Cart from '@/components/Cart'
+import SingleMenuItem from '@/components/parts/SingleMenuItem'
+import ListControll from '@/components/ListControll'
 
 export default {
 
   data () {
     return {
-
+      menuItems: []
     }
   },
 
   methods: {
-
+    async getMenuItems () {
+      const { data } = await cateringService.getMentItemsForCatering(this.$route.params.id)
+      this.menuItems = data.docs
+    }
   },
 
   computed: {
 
   },
 
+  created () {
+    this.getMenuItems()
+  },
+
   components: {
-    Menu, Cart
+    Cart, ListControll, SingleMenuItem
   }
 }
 </script>
