241a2b92c3321abe1ca0fcf2021fcc9ac628703b
Mihailo Tomisic <mihailotomisic@MacBook-Pro-6.local>
2019-07-21 21:20:58 +0200

    dreams and hopes

--- src/views/AppLanding.vue
+++ src/views/AppLanding.vue
@@ -1,6 +1,36 @@
 <template>
-  <div>
-    <div>Landing page</div>
-    <div>Welcome</div>
-  </div>
+  <b-container>
+
+    <h1>
+      Welcome
+    </h1>
+
+    <div class="my-4">
+      We bring you all the news about cusine in your area. In order to use all the available features,
+      please make account and log in. The most recent news:
+    </div>
+
+    <b-container class="mt-2">
+      <div>Last week there were 22 new foody places.</div>
+    </b-container>
+
+    <b-container class="mt-2">
+      <div>Last week there were 232 new reviews.</div>
+    </b-container>
+
+    <b-container class="mt-2">
+      <div>In addition, last week there were 422 registered new users.</div>
+    </b-container>
+
+  </b-container>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
